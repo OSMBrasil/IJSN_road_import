18e87eb3988fa33e17d19eb2e6f225f84da53ebf
Wille Marcel <wille@wille.blog.br>
2015-02-12 12:55:33 -0200

    add license and fix folders

--- import/convert_shapes.sh
+++ import/convert_shapes.sh
@@ -1,20 +1,38 @@
 #!/bin/sh
+# This script converts a shapefile to WGS84, selecting fields and streets
 
-unzip Arruamento.zip -d streets
+# Copyright (C) 2015  Wille Marcel <wille@wille.blog.br>
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+unzip ../shp/Arruamento.zip -d ../shp/streets
 #unzip Trecho_Rodoviario.zip -d highways
 
 # convert to WGS84, selecting fields and streets with name value
 ogr2ogr -select 'nome, revestimen, nrFaixas, municipio' \
     -a_srs EPSG:4326 -t_srs EPSG:4326 \
     -where 'nome is not null' \
-    -f 'ESRI Shapefile' streets/streets.shp \
-    streets/Arruamento.shp
+    -f 'ESRI Shapefile' ../shp/streets/streets.shp \
+    ../shp/streets/Arruamento.shp
 
 # convert to WGS84, selecting fields and streets with no name value
 ogr2ogr -select 'nome, revestimen, nrFaixas, municipio' \
     -a_srs EPSG:4326 -t_srs EPSG:4326 \
     -where 'nome is null' \
-    -f 'ESRI Shapefile' streets/streets-no-name.shp \
-    streets/Arruamento.shp
+    -f 'ESRI Shapefile' ../shp/streets/streets-no-name.shp \
+    ../shp/streets/Arruamento.shp
 
-rm streets/Arruamento.*
+rm ../shp/streets/Arruamento.*
